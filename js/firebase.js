0bbedfcde11398f282bc2fbd916cf8db42c1a1b7
NishiGaming <nishikantlataye750@gmail.com>
2026-03-28 00:59:52 +0530

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBQQc4eNqSXqGOJMMPzjQwOLPcVAO-NRQE",
+  authDomain: "nishi-gaming-hub.firebaseapp.com",
+  projectId: "nishi-gaming-hub",
+  storageBucket: "nishi-gaming-hub.firebasestorage.app",
+  messagingSenderId: "326151952421",
+  appId: "1:326151952421:web:9e79fed644a281328f47cc"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
