f8806740f8f4b91e5a4337deeab4ae8ea19b00f2
NishiGaming <nishikantlataye750@gmail.com>
2026-03-28 02:12:52 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,4 @@
+// 1. Tera Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyBQQc4eNqSXqGOJMMPzjQwOLPcVAO-NRQE",
   authDomain: "nishi-gaming-hub.firebaseapp.com",
@@ -11,8 +12,32 @@ const firebaseConfig = {
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
-
 const auth = firebase.auth();
-const db = firebase.firestore();
 
-console.log("Firebase initialized successfully for Nishi Hub!");
+// 2. FLAG SETUP (Isse Flag dikhega)
+const input = document.querySelector("#phone_input");
+const iti = window.intlTelInput(input, {
+    initialCountry: "in", // India default
+    separateDialCode: true, // Flag ke sath +91 alag dikhega
+    utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
+
+// 3. Recaptcha (OTP ke liye zaroori)
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
+    'size': 'invisible'
+});
+
+// 4. Send OTP Function
+window.sendOTP = function() {
+    const phoneNumber = iti.getNumber(); // Ye apne aap +91 jod dega
+    const appVerifier = window.recaptchaVerifier;
+
+    auth.signInWithPhoneNumber(phoneNumber, appVerifier)
+        .then((confirmationResult) => {
+            window.confirmationResult = confirmationResult;
+            alert("OTP Bhej diya gaya hai!");
+        }).catch((error) => {
+            console.error("Error:", error);
+            alert("Galti: " + error.message);
+        });
+}
